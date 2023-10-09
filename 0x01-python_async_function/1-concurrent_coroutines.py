0583f10bbbf6581845555102f5b0a7bc6db9f90a
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-09 17:08:42 +0300

    execute multiple coroutines at the same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 """create a function to execute multiple coroutines at the same time with async"""
 
 
