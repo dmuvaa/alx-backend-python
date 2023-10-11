82bf75c88b4effc34bf222109ea871146b0468a5
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-11 20:40:30 +0300

    coroutine called async_generator that takes no arguments

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,12 +4,13 @@
 
 import asyncio
 import random
+from typing import List
 
 
 """Create a Function for the async generator"""
 
 
-async def async_generator():
+async def async_generator() -> List[float]:
     """coroutine will loop 10 times, each time asynchronously wait 1 second"""
     for _ in range(10):
         await asyncio.sleep(1)
