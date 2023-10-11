0dee68aa9035b72e76ecf91b2a331506c33a3225
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-11 21:03:26 +0300

    function that should measure the total runtime and return it

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -3,7 +3,7 @@
 """Import both async_generator and async_comprehension modules"""
 
 import asyncio
-from time import time
+import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
@@ -12,7 +12,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime():
     """Function that measures the run time for the coroutines"""
-    start_time = time()
-    await asyncio.gather(*(async_comprehension()for _ in range(4)))
-    end_time = time()
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.time()
     return end_time - start_time
