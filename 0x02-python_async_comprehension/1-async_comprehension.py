3acc70b0e28a802f54597d0069f2107a8d13bb28
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-11 20:38:19 +0300

    Async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,14 @@
 
 """Import the async_generator from 0-async_generator"""
 
+import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 """Create a function known as async comprehension"""
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Function that creates async comprehension"""
     return [i async for i in async_generator()]
